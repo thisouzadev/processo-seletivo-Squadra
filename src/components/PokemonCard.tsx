b7c557e4a3e622a9da9fb5d8294880da1f2a695c
thisouza <thiagodesouza.dev@gmail.com>
2024-06-25 19:11:46 -0300

    cores nos card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -8,6 +8,27 @@ import { Pokemon } from '../pages/types'
 import { IconButton } from '@mui/material'
 import { Favorite } from '@mui/icons-material'
 
+const typeColors: { [key: string]: string } = {
+  normal: '#A8A77A',
+  fire: '#EE8130',
+  water: '#6390F0',
+  electric: '#F7D02C',
+  grass: '#7AC74C',
+  ice: '#96D9D6',
+  fighting: '#C22E28',
+  poison: '#A33EA1',
+  ground: '#E2BF65',
+  flying: '#A98FF3',
+  psychic: '#F95587',
+  bug: '#A6B91A',
+  rock: '#B6A136',
+  ghost: '#735797',
+  dragon: '#6F35FC',
+  dark: '#705746',
+  steel: '#B7B7CE',
+  fairy: '#D685AD',
+}
+
 interface PokemonCardProps {
   pokemon: Pokemon
 }
@@ -56,9 +77,12 @@ const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
     updateFavoritePokemons(pokemon.id, !isFavorite)
     setIsFavorite(!isFavorite)
   }
+
+  const color = typeColors[pokemon.details!.types[0]]
+
   return (
     <Grid item xs={12} sm={6} md={4}>
-      <Card>
+      <Card style={{ backgroundColor: color, color: '#FFF' }}>
         {pokemon.details && (
           <>
             <IconButton
